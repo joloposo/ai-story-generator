e6b33d9587380bb943e3b549fa715705ebe5557a
Michal Vavrek <vavrek@duxsoft.sk>
2024-09-29 22:22:53 +0200

    Text to speech

--- app/view-story/[id]/page.tsx
+++ app/view-story/[id]/page.tsx
@@ -1,15 +1,20 @@
 'use client';
 import { db } from '@/config/db';
 import { StoryData } from '@/config/schema';
-import React, { use, useEffect, useState } from 'react';
+import React, { use, useEffect, useState, useRef } from 'react';
 import { eq } from 'drizzle-orm';
 import HTMLFlipBook from 'react-pageflip';
 import BookCoverPage from '../_components/BookCoverPage';
 import StoryPages from '../_components/StoryPages';
 import LastPage from '../_components/LastPage';
+import { Button } from '@nextui-org/button';
+import { IoIosArrowDroprightCircle, IoIosArrowDropleftCircle } from 'react-icons/io';
 
 function ViewStory({ params }: any) {
     const [story, setStory] = useState<any>();
+    const [count, setCount] = useState(0);
+    const book = useRef();
+
     useEffect(() => {
         console.log('params.id', params.id);
         getStory();
@@ -23,23 +28,56 @@ function ViewStory({ params }: any) {
     };
 
     return (
-        <div className='p-10 md:px-20 lg:px-40'>
+        <div className='p-10 md:px-20 lg:px-40 flex-col'>
             <h2 className='font-bold text-4xl bg-primary text-center p-10 text-white'>{story?.output?.story?.title}</h2>
-
-            {/* @ts-ignore */}
-            <HTMLFlipBook width={500} height={500} showCover={true} className='mt-10'>
-                <div>
-                    <BookCoverPage imageUrl={story?.coverImage} />
-                </div>
-                {[...Array(story?.output?.story?.chapters.length)].map((item, index) => (
-                    <div key={index} className='bg-white p-10 border'>
-                        <StoryPages storyChapter={story?.output?.story?.chapters[index]} />
+            <div className='relative'>
+                {/* @ts-ignore */}
+                <HTMLFlipBook
+                    width={500}
+                    height={500}
+                    showCover={true}
+                    className='mt-10'
+                    useMouseEvents={false}
+                    ref={book}
+                >
+                    <div>
+                        <BookCoverPage imageUrl={story?.coverImage} />
                     </div>
-                ))}
-                {/* <div>
+                    {[...Array(story?.output?.story?.chapters.length)].map((item, index) => (
+                        <div key={index} className='bg-white p-10 border'>
+                            <StoryPages storyChapter={story?.output?.story?.chapters[index]} />
+                        </div>
+                    ))}
+
+                    {/* <div>
                     <LastPage />
                 </div> */}
-            </HTMLFlipBook>
+                </HTMLFlipBook>
+                {count != 0 && (
+                    <div
+                        className='absolute -left-6 top-[250px]'
+                        onClick={() => {
+                            // @ts-ignore
+                            book.current.pageFlip().flipPrev();
+                            setCount(count - 1);
+                        }}
+                    >
+                        <IoIosArrowDropleftCircle className='text-5xl text-primary cursor-pointer' />
+                    </div>
+                )}
+                {count != (story?.output?.story?.chapters.length-1) && (
+                    <div
+                        className='absolute -right-6 top-[250px]'
+                        onClick={() => {
+                            // @ts-ignore
+                            book.current.pageFlip().flipNext();
+                            setCount(count + 1);
+                        }}
+                    >
+                        <IoIosArrowDroprightCircle className='text-5xl text-primary cursor-pointer' />
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
--- app/view-story/_components/StoryPages.tsx
+++ app/view-story/_components/StoryPages.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { MdPlayCircle } from "react-icons/md";
 
 function StoryPages({ storyChapter }: any) {
+
+    const playSpeech = (text: string) => {
+        const synth = window.speechSynthesis;
+        const textToSpeech = new SpeechSynthesisUtterance(text); 
+        synth.speak(textToSpeech);
+    }
+
     return (
         <div>
-            <h2 className='text-2xl font-bold text-primary'>{storyChapter?.title}</h2>
+            <h2 className='text-2xl font-bold text-primary flex justify-between'>{storyChapter?.title}
+              <span className='text-3xl cursor-pointer' onClick={()=>playSpeech(storyChapter?.text)}><MdPlayCircle /></span>
+            </h2>
             <p className='text-xl p-10 mt-3 rounded-lg bg-slate-100 text-black'>{storyChapter?.text}</p>
         </div>
     );
