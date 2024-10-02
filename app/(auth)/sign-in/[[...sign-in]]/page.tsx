609413a76ddf6ab57e4d9462f4d0c65e984954f8
joloposo <michal.vavrek@coratec.sk>
2024-10-02 10:16:45 +0200

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 bg-emerald-600'>
       <div>
-        <Image src={'public/static/images/login.png'} alt='login' width={700} height={1000}   />
+        <Image src={'/static/images/login.png'} alt='login' width={700} height={1000}   />
       </div>
       <div className='flex justify-center items-center h-screen order-first md:order-last'>
         <SignIn />
